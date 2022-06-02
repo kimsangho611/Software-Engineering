d5d3a218d0558137d9bd67dd8e64e87bcb6b78e6
ssu00 <rachel4535@naver.com>
2022-06-02 14:52:47 +0900

    💄 asset(icon, sample) 추가

---
+++ src/assets/icons/index.jsx
@@ -0,0 +1,5 @@
+import { ReactComponent as IC_Hamburger_Bar } from "./hamburger_bar.svg";
+import { ReactComponent as IC_Logo } from "./logo.svg";
+import { ReactComponent as IC_Search } from "./search.svg";
+import { ReactComponent as IC_Heart } from "./heart.svg";
+export { IC_Hamburger_Bar, IC_Logo, IC_Search,IC_Heart };
