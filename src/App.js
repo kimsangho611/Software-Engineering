6374c8d2fe061badbbdb437cad30bad90fc53fc8
zoowb <zoowb0829@gmail.com>
2022-06-06 14:26:34 +0900

    Revert ":lipstick: update home"
    This reverts commit 8e7084eb406802d4f5981a61cd5be85fb1af734a.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home/home";
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
