8d53eddd778beb1c7784f8bff1b128215df7d6bc
zoowb <zoowb0829@gmail.com>
2022-06-06 19:43:09 +0900

    :zap: 라우트, 부트스트랩 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/home";
+import Login from "./pages/auth/login";
+import SignUp from "./pages/auth/signup";
+import Drop from "./pages/auth/drop";
+import DropSuccess from "./pages/auth/dropSuccess";
+import Search from "./pages/search/search";
 
 const App = () => {
   return (
@@ -8,6 +13,11 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/drop" element={<Drop />} />
+          <Route path="/drop/success" element={<DropSuccess />} />
+          <Route path="/search" element={<Search />} />
         </Routes>
       </BrowserRouter>
     </div>
