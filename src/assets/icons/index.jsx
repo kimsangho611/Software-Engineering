1d16deb25f08cfc918a31e4e5c00e29249da1ac9
ssu00 <rachel4535@naver.com>
2022-06-06 12:40:41 +0900

    💄 asset(icon) 추가

--- src/assets/icons/index.jsx
+++ src/assets/icons/index.jsx
@@ -2,4 +2,30 @@ import { ReactComponent as IC_Hamburger_Bar } from "./hamburger_bar.svg";
 import { ReactComponent as IC_Logo } from "./logo.svg";
 import { ReactComponent as IC_Search } from "./search.svg";
 import { ReactComponent as IC_Heart } from "./heart.svg";
-export { IC_Hamburger_Bar, IC_Logo, IC_Search,IC_Heart };
+import { ReactComponent as IC_Siren } from "./siren.svg";
+import { ReactComponent as IC_Person } from "./person.svg";
+import { ReactComponent as IC_Shopping_Bag } from "./shopping_bag.svg";
+import { ReactComponent as IC_Shop } from "./shop.svg";
+import { ReactComponent as IC_Heart_In_Circle } from "./heart_in_circle.svg";
+import { ReactComponent as IC_Counselor } from "./counselor.svg";
+import { ReactComponent as IC_Piggy_Bank } from "./piggy_bank.svg";
+import { ReactComponent as IC_Speaker } from "./speaker.svg";
+import { ReactComponent as IC_CheckBox } from "./checkbox.svg";
+import { ReactComponent as IC_LogOut } from "./logout.svg";
+
+export {
+  IC_Hamburger_Bar,
+  IC_Logo,
+  IC_Search,
+  IC_Heart,
+  IC_Siren,
+  IC_Person,
+  IC_Shopping_Bag,
+  IC_Shop,
+  IC_Heart_In_Circle,
+  IC_Counselor,
+  IC_Piggy_Bank,
+  IC_Speaker,
+  IC_CheckBox,
+  IC_LogOut,
+};
