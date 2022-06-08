3e0ceb508f6b998468c65d1335135b9725b0eaef
zoowb <zoowb0829@gmail.com>
2022-06-08 21:28:07 +0900

    :zap: 회원 탈퇴 api 추가

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,13 +4,8 @@ const jwt = require("../module/jwt");
 const pool = require("../module/mysql");
 const pool2 = require("../module/mysql2");
 
-router.post("/", function (req, res, next) {
-  res.send({ msg: "안녕" });
-});
-
 router.post("/login/:type", function (req, res, next) {
   const type = req.params.type;
-  console.log("type!!!!!", type);
   var email = req.body.email;
   var password = req.body.password;
   var datas = [email, password];
@@ -25,9 +20,6 @@ router.post("/login/:type", function (req, res, next) {
         res.status(500).send({ success: false, err: "DB 오류" + err });
         console.error("err : " + err);
       } else {
-        console.log("sql==", sql);
-        console.log("data==", datas);
-        console.log("result==", result);
         if (result.length > 0) {
           const jwtToken = await jwt.sign({
             id: result[0].u_email,
@@ -46,10 +38,7 @@ router.post("/login/:type", function (req, res, next) {
 });
 
 router.post("/signup", async function (req, res, next) {
-  var email = req.body.email;
-  var password = req.body.password;
-  var name = req.body.name;
-  var phone = req.body.phone;
+  const { email, password, name, phone } = req.body;
   var datas = [email, password, name, phone];
 
   const connection = await pool2.getConnection(async (conn) => conn);
@@ -58,7 +47,6 @@ router.post("/signup", async function (req, res, next) {
       "select * from user where u_email=?;",
       [email]
     );
-    console.log("result signup==", result1);
     const data1 = result1[0];
     if (data1.length > 0) {
       res.status(401).send({
@@ -70,6 +58,37 @@ router.post("/signup", async function (req, res, next) {
         "insert into user(u_email, u_pw, u_name, u_phone) values (?,?,?,?);";
       await connection.query(sql, datas);
       res.status(200).send({ success: true, msg: "회원가입 성공" });
+    }
+  } catch (err) {
+    res.status(500).send({ success: false, msg: "서버 오류" + err });
+  } finally {
+    connection.release();
+  }
+});
+
+router.delete("/drop", async function (req, res, next) {
+  const { email, password } = req.body;
+  var datas = [email, password];
+
+  const connection = await pool2.getConnection(async (conn) => conn);
+  try {
+    const result1 = await connection.query(
+      "SELECT * FROM user WHERE u_email=? and u_pw=?;",
+      datas
+    );
+    if (result1.length > 0) {
+      await connection.query(
+        "delete from user where u_email =? and u_pw =?;",
+        datas
+      );
+      res
+        .status(200)
+        .send({ success: true, msg: "회원 탈퇴가 완료되었습니다." });
+    } else {
+      res.status(401).send({
+        success: false,
+        msg: "이메일 또는 비밀번호가 일치하지 않습니다.",
+      });
     }
   } catch (err) {
     res.status(500).send({ success: false, msg: "서버 오류" + err });
