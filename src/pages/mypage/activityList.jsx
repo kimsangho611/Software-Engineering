97e41c7459a739be86389d552608853d211b1685
zoowb <zoowb0829@gmail.com>
2022-06-10 03:37:14 +0900

    :construction: 활동목록 작업중

--- src/pages/mypage/activityList.jsx
+++ src/pages/mypage/activityList.jsx
@@ -1,62 +1,58 @@
+import { useEffect, useState } from "react";
 import ProductBlock from "../../components/common/productBlock";
 import { Layout } from "../../components/layout";
-import {
-  MypageProductCard,
-  MypageProductCardWPrice,
-} from "../../components/mypage/mypageCard";
+import { GetActivityApi } from "../../core/api/mypage/getActivity";
 import styles from "./list.module.scss";
-import product from "../../assets/samples/product.png";
 
 const MyPageActivityList = () => {
-  const productInfo = [
-    {
-      img: product,
-      like: 1200,
-      firstCate: "WOMEN",
-      secondCate: "치마",
-      title: "치마 팝니다",
-      view: 4302,
-      price: 18000,
-      state: "판매 중",
-    },
-    {
-      img: product,
-      like: 200,
-      firstCate: "MEN",
-      secondCate: "바지",
-      title: "바지 팝니다",
-      view: 102,
-      price: 26000,
-      state: "거래 완료",
-    },
-    {
-      img: product,
-      like: 121,
-      firstCate: "WOMEN",
-      secondCate: "니트",
-      title: "니트 팝니다",
-      view: 412,
-      price: 41000,
-      state: "판매 중",
-    },
+  const [productInfo, setProductInfo] = useState([]);
+  const [order, setOrder] = useState(1);
+  const OPTIONS = [
+    { value: 1, name: "찜한 게시물" },
+    { value: 2, name: "댓글 단 게시물" },
   ];
+  const handleChange = (e) => {
+    setOrder(e.target.value);
+  };
+
+  const GetProductInfo = async () => {
+    const res = await GetActivityApi(order);
+    setProductInfo(res.result);
+  };
+
+  useEffect(() => {
+    GetProductInfo();
+  }, [order]);
   return (
     <Layout otherClass={styles.mypageBuyList}>
       <h1 className={styles.title}>판매 목록</h1>
-      <select>
-        <option>찜한 게시물</option>
-        <option>댓글 단 게시물</option>
+      <select onChange={handleChange}>
+        {OPTIONS.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.name}
+          </option>
+        ))}
       </select>
 
       <div className={styles.lists}>
-        {productInfo.map((data, i) => {
-          return (
-            <div className={styles.card} key={i}>
-              <span className={styles.date}>{data.date}</span>
-              <MypageProductCardWPrice productInfo={data} />
-            </div>
-          );
-        })}
+        <div className="productOneLine">
+          {productInfo.map((item, key) => {
+            return (
+              <ProductBlock
+                key={key}
+                id={item?.p_id}
+                img={item?.p_image}
+                like={item?.likecnt}
+                firstCate={item?.p_category1}
+                secondCate={item?.p_category2}
+                title={item?.p_title}
+                view={item?.p_view}
+                price={item?.p_price}
+                state={item?.p_trade}
+              />
+            );
+          })}
+        </div>
       </div>
     </Layout>
   );
